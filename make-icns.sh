47db63bca746fcb897a41d8776745ccac54d2ea1
0u812 <0u812@users.noreply.github.com>
2017-05-10 17:58:54 -0700

    remove 64 icons

--- make-icns.sh
+++ make-icns.sh
@@ -9,7 +9,13 @@ set -o verbose
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 THIS_DIR=`pwd`
 
+# http://stackoverflow.com/questions/11660736/xcode-4-4-removed-icon-composer-in-developer-tools
+
 rm -rf tellurium.iconset
 mkdir -p tellurium.iconset
+# rsync
 rsync -av telogo6/*.png tellurium.iconset
+for f in tellurium.iconset/*.png; do
+  mv "$f" "${f/tellurium_/icon_}"
+done
 iconutil -c icns tellurium.iconset
